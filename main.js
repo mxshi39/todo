e5e0a381a72078531a34cfbdbb27871018b64731
mxshi39 <43346879+mxshi39@users.noreply.github.com>
2018-09-25 19:38:51 -0400

    Lab excercise

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ function addNewList() {
 
 function addListItem() {
     console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+	newItem.appendChild(document.createTextNode(itemInput.value));
+	list.appendChild(newItem);
+
 }
